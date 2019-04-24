8c75513525a090d11cb687d198e50f0d473322fb
Ben Governali <bgovern93@gmail.com>
2019-04-24 15:45:13 -0400

    monster now wraps around edges of display rather than move off of it. Seems to only work once though, monster running of right edge after first wrap around

--- monster_game.py
+++ monster_game.py
@@ -1,10 +1,11 @@
 import pygame
 
 class Character(pygame.sprite.Sprite):
-    def __init__(self, image, speed, x, y):
+    def __init__(self, image, speed_x, speed_y, x, y):
         pygame.sprite.Sprite.__init__(self)
         self.image = image
-        self.speed = speed
+        self.speed_x = speed_x
+        self.speed_y = speed_y
         self.x = x
         self.y = y
         self.rect = self.image.get_rect()
@@ -17,9 +18,23 @@ class Goblin(Character):
     pass
 
 class Monster(Character):
-    def move(self):
-        self.x += self.speed
+    def move(self, width, height):
+        self.x += self.speed_x
+        self.y += self.speed_y
         self.rect.center = [self.x, self.y]
+        if self.x > width:
+            self.speed_x = 0
+            self.speed_y = 5
+        if self.y > height:
+            self.speed_x = -5
+            self.speed_y = 0
+        if self.x < 0:
+            self.speed_x = 0
+            self.speed_y = -5
+        if self.y < 0:
+            self.speed_x = 5
+            self.speed_y = 0
+        print(self.x, self.y)
 
 def main():
     width = 512
@@ -35,8 +50,8 @@ def main():
     monster_image = pygame.image.load('images/monster.png').convert_alpha()
 
     # Our hero and monster
-    player = Hero(hero_image, 5, 250, 250)
-    monster = Monster(monster_image, 5, 150, 120)
+    player = Hero(hero_image, 0, 0, 250, 250)
+    monster = Monster(monster_image, 5, 0, 150, 120)
 
     # Create groups for monster and player to apply draw attribute
     player_group = pygame.sprite.Group()
@@ -57,7 +72,7 @@ def main():
 
 
         # Game logic
-        monster.move()
+        monster.move(width, height)
 
         # Draw background
         screen.fill(blue_color)
